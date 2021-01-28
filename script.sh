c78ff2d509dc8cd79d678d95a485c9f3db2aa421
7db9a <repo.7db9a@gmail.com>
2021-01-27 21:45:25 -0500

    dev: add script to build desktop environment.

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# DON"T EXCUTE THIS SCRIPT.
+
+# Install Nix
+
+# Install xbindkeys
+
+# Symlink individualized man page to where man can find it.
+sudo ln -s ~/.local/i-man/man1/xbindkeys.1 /usr/local/share/man/man1/i-xbindkeys.1
