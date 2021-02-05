d66dab357e7066be2d4452ac58539529f31d1ae8
7db9a <repo.7db9a@gmail.com>
2021-02-04 22:40:57 -0500

    dev(script): add nix install and first nix pkg.

--- script.sh
+++ script.sh
@@ -3,8 +3,13 @@
 # DON"T EXCUTE THIS SCRIPT.
 
 # Install Nix
+# nixos.org/downloads.html#nix-quick-install
+curl -L https://nixos.org/nix/install/sh
+# Otherwise nix-env and others will appear not to be on the system.
+source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 # Install xbindkeys
+nix-env iA nixpkgs.xbindkeys
 
 # Symlink individualized man page to where man can find it.
-sudo ln -s ~/.local/i-man/man1/xbindkeys.1 /usr/local/share/man/man1/i-xbindkeys.1
+sudo ln -s $HOME/:w.local/i-man/man1/xbindkeys.1 /usr/local/share/man/man1/i-xbindkeys.1
